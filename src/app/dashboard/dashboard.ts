de7907936b69cf763dfbbefd5ec2243c8e42e35d
Sneha Mishra <sneham4758@gmail.com>
2026-06-29 20:15:29 +0530

    style: upgrade dashboard with Angular Material

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterLink],
+  imports: [
+  CommonModule,
+  MatCardModule,
+  MatButtonModule,
+  MatIconModule,
+  RouterLink
+],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
